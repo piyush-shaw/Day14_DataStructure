52e4d5523fecca224d7e34c6e44e69c80f34d0b8
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-18 22:04:04 +0530

    [Piyush] added to create all operation on stack using linked list

---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackUsingLinkedList;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        LinkedListStack linkedListStack = new LinkedListStack();
+        linkedListStack.Push(70);
+        linkedListStack.Push(30);
+        linkedListStack.Push(56);
+        linkedListStack.Display();
+        //linkedListStack.Peek();
+        //linkedListStack.Pop();
+        //linkedListStack.IsEmpty();
+        //linkedListStack.Display();
+        Console.ReadKey();
+    }
+}
